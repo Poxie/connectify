de71179bebefbb608592d96a317cd7085df291aa
Poxie <poxen12@hotmail.com>
2022-09-16 23:07:36 +0200

    Added skeleton to user header

--- frontend/layouts/user/UserHeader.tsx
+++ frontend/layouts/user/UserHeader.tsx
@@ -3,11 +3,12 @@ import { useAppSelector } from '../../redux/store';
 import { selectUserById } from '../../redux/users/selectors';
 import styles from '../../styles/User.module.scss';
 import { UserHeaderButtons } from "./UserHeaderButtons";
+import { UserHeaderSkeleton } from './UserHeaderSkeleton';
 
 export const UserHeader = () => {
     const { userId } = useRouter().query as { userId: string };
     const user = useAppSelector(state => selectUserById(state, parseInt(userId)));
-    if(!user) return null;
+    if(!user) return <UserHeaderSkeleton />;
 
     return(
         <div className={styles['header']}>
