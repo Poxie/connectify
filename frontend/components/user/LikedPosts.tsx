826937fa4b5985035dd1de263bb26948d4484699
Poxie <poxen12@hotmail.com>
2022-09-16 23:41:48 +0200

    Displaying user liked posts

--- frontend/components/user/LikedPosts.tsx
+++ frontend/components/user/LikedPosts.tsx
@@ -1,32 +1,32 @@
 import styles from '../../styles/User.module.scss';
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useAuth } from "../../contexts/auth/AuthProvider";
 import { setPosts } from "../../redux/posts/actions";
-import { useAppSelector } from "../../redux/store"
-import { setUserPostIds } from "../../redux/users/actions";
-import { selectUserExists, selectUserHasLoadedPosts, selectUserPostIds } from "../../redux/users/selectors";
+import { useAppSelector } from "../../redux/store";
+import { setUserLikedIds, setUserPostIds } from "../../redux/users/actions";
+import { selectUserExists, selectUserHasLoadedLikedPosts, selectUserLikedIds } from "../../redux/users/selectors";
 import { Post } from "../../types";
-import { ProfilePost } from "./ProfilePost";
-import { UserPostSkeleton } from '../user-post/UserPostSkeleton';
+import { UserPostSkeleton } from "../user-post/UserPostSkeleton";
+import { ProfilePost } from './ProfilePost';
 
-export const UserProfile = () => {
+export const LikedPosts = () => {
     const { get } = useAuth();
     const dispatch = useDispatch();
     const { userId } = useRouter().query as { userId: string };
     const userExists = useAppSelector(state => selectUserExists(state, parseInt(userId)));
-    const postIds = useAppSelector(state => selectUserPostIds(state, parseInt(userId)));
-    const userHasLoadedPosts = useAppSelector(state => selectUserHasLoadedPosts(state, parseInt(userId)));
+    const postIds = useAppSelector(state => selectUserLikedIds(state, parseInt(userId)));
+    const userHasLoadedPosts = useAppSelector(state => selectUserHasLoadedLikedPosts(state, parseInt(userId)));
 
     // Fetching user posts
     useEffect(() => {
         if(!userId || !userExists || userHasLoadedPosts) return;
 
-        get(`/users/${userId}/posts`)
+        get(`/users/${userId}/likes`)
             .then((posts: Post[]) => {
                 dispatch(setPosts(posts));
-                dispatch(setUserPostIds(parseInt(userId), posts.map(post => post.id)))
+                dispatch(setUserLikedIds(parseInt(userId), posts.map(post => post.id)))
             })
     }, [userId, userExists, userHasLoadedPosts]);
 
@@ -43,14 +43,14 @@ export const UserProfile = () => {
         <div className={styles['posts']}>
             {postIds.length === 0 && (
                 <span className={styles['empty']}>
-                    User has not posted anything.
+                    User has not liked any posts.
                 </span>
             )}
 
-            {postIds.map(postId => (
+            {postIds.map(id => (
                 <ProfilePost 
-                    postId={postId}
-                    key={postId}
+                    postId={id}
+                    key={id}
                 />
             ))}
         </div>
--- frontend/pages/users/[userId]/liked-posts.tsx
+++ frontend/pages/users/[userId]/liked-posts.tsx
@@ -1,15 +1,16 @@
 import { ReactElement } from "react"
+import { LikedPosts } from "../../../components/user/LikedPosts"
 import { UserLayout } from "../../../layouts/user/UserLayout"
 import { NextPageWithLayout } from "../../_app"
 
-const LikedPosts: NextPageWithLayout = () => {
-    return <div>liked posts</div>
+const Liked: NextPageWithLayout = () => {
+    return <LikedPosts />
 }
 
-LikedPosts.getLayout = (page: ReactElement) => (
+Liked.getLayout = (page: ReactElement) => (
     <UserLayout>
         {page}
     </UserLayout>
 )
 
-export default LikedPosts;
+export default Liked;
