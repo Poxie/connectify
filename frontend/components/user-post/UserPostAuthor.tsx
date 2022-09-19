4333d4ec7ea91f105e1f65731e491f144bfc67b8
Poxie <poxen12@hotmail.com>
2022-09-19 22:32:48 +0200

    If avatar present, display avatar on post

--- frontend/components/user-post/UserPostAuthor.tsx
+++ frontend/components/user-post/UserPostAuthor.tsx
@@ -1,13 +1,27 @@
+import Image from "next/image";
 import Link from "next/link";
 import { User } from "../../types"
 import styles from './UserPost.module.scss';
 
-export const UserPostAuthor: React.FC<User> = ({ id, display_name, username }) => {
+export const UserPostAuthor: React.FC<User> = ({ id, display_name, username, avatar }) => {
     return(
+        <>
+        <Link href={`/users/${id}`}>
+            <a className={styles['author-avatar']}>
+                {avatar && (
+                    <Image 
+                        src={`${process.env.NEXT_PUBLIC_AVATAR_ENDPOINT}${avatar}`}
+                        width={25}
+                        height={25}
+                    />
+                )}
+            </a>
+        </Link>
         <Link href={`/users/${id}`}>
             <a className={styles['author-name']}>
                 {display_name || username}
             </a>
         </Link>
+        </>
     )
 }
