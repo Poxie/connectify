5b2d5eaa28ef6c0e5815a031db760bea6f34216c
Poxie <poxen12@hotmail.com>
2022-09-19 23:08:12 +0200

    Responsive main layout

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.scss'
+import styles from '../styles/Main.module.scss';
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '../contexts/auth/AuthProvider'
 import { Navbar } from '../components/navbar'
@@ -24,18 +25,9 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
     <AuthProvider>
       <ModalProvider>
         <Navbar />
-        <div style={{
-          display: 'flex',
-          width: 'var(--width-main)',
-          maxWidth: 'var(--width-max)',
-          margin: '0 auto'
-        }}>
+        <div className={styles['app-content']}>
           <Sidebar />
-          <main style={{
-            flex: 1,
-            paddingLeft: 'var(--spacing-primary)',
-            paddingTop: 'var(--spacing-primary)'
-          }}>
+          <main>
             {getLayout(
               <Component {...pageProps} />
             )}
