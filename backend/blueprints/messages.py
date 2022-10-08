cb0021307895e4d54dd967a791a0ea484c819f22
Poxie <poxen12@hotmail.com>
2022-10-08 16:33:24 +0200

    Added ability to create direct messages

--- backend/blueprints/messages.py
+++ backend/blueprints/messages.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
-from utils.messages import get_channel_messages
+from flask import Blueprint, jsonify, request
+from utils.messages import get_channel_messages, create_channel_message
+from utils.auth import token_required
 
 messages = Blueprint('messages', __name__)
 
@@ -7,3 +8,18 @@ messages = Blueprint('messages', __name__)
 def get_messages(channel_id: int):
     messages = get_channel_messages(channel_id)
     return jsonify(messages)
+
+@messages.post('/channels/<int:channel_id>/messages')
+@token_required
+def add_message(channel_id: int, token_id: int):
+    content = request.form.get('content')
+
+    # Creating message
+    data = {
+        'channel_id': channel_id,
+        'author_id': token_id,
+        'content': content
+    }
+    message = create_channel_message(data)
+
+    return jsonify(message)
--- backend/utils/messages.py
+++ backend/utils/messages.py
@@ -1,5 +1,27 @@
 from database import db
+from random import randrange
+import time
 
+# Creating message id
+MESSAGE_ID_LENGTH = 10
+def create_message_id() -> int:
+    opts = '0123456789'
+    
+    # Creating random id
+    id = ''
+    for i in range(MESSAGE_ID_LENGTH):
+        id += opts[randrange(len(opts))]
+    id = int(id)
+
+    # Checking if id already exists
+    message = get_message_by_id(id)
+    if message:
+        return create_message_id()
+
+    # Else return created id
+    return id
+
+# Getting messages
 def get_channel_messages(channel_id: int):
     # Creating select query
     query = "SELECT * FROM messages WHERE channel_id = %s"
@@ -9,3 +31,37 @@ def get_channel_messages(channel_id: int):
     messages = db.fetch_all(query, values)
 
     return messages
+
+# Getting specific message
+def get_message_by_id(id: int):
+    # Creating select query
+    query = "SELECT * FROM messages WHERE id = %s"
+    values = (id,)
+
+    # Executing query
+    message = db.fetch_one(query, values)
+
+    return message
+
+# Creating message
+def create_channel_message(message):
+    # Creating id
+    id = create_message_id()
+
+    # Creating insert query
+    query = "INSERT INTO messages (id, channel_id, author_id, content, timestamp) VALUES (%s, %s, %s, %s, %s)"
+    values = (
+        id,
+        message['channel_id'],
+        message['author_id'],
+        message['content'],
+        time.time()
+    )
+
+    # Executing insert query
+    db.insert(query, values)
+
+    # Fetching created message
+    message = get_message_by_id(id)
+    
+    return message
