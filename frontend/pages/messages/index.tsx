f1fe329b062a8081c80211e0b86c3b246ad95542
Poxie <poxen12@hotmail.com>
2022-10-10 21:13:47 +0200

    Added default screen to messages page

--- frontend/pages/messages/index.tsx
+++ frontend/pages/messages/index.tsx
@@ -1,11 +1,21 @@
+import styles from '../../styles/Messages.module.scss';
 import { ReactElement } from "react";
 import { MessagesLayout } from "../../layouts/messages/MessagesLayout";
 import { NextPageWithLayout } from "../_app";
+import Button from '../../components/button';
 
 const Messages: NextPageWithLayout = () => {
     return(
-        <div>
-            messages
+        <div className={styles['empty']}>
+            <h2>
+                Choose a direct message
+            </h2>
+            <span>
+                Choose an existing direct message or create a new one.
+            </span>
+            <Button className={styles['conversation-button']}>
+                Start conversation
+            </Button>
         </div>
     )
 }
