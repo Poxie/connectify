f9439bd26e750b136c3537144574eb456f100ddb
Poxie <poxen12@hotmail.com>
2022-10-17 23:56:38 +0200

    Added basic user menu

--- frontend/components/navbar/NavbarProfile.tsx
+++ frontend/components/navbar/NavbarProfile.tsx
@@ -1,9 +1,46 @@
+import styles from '../../styles/Navbar.module.scss';
+import Image from "next/image"
 import { User } from "../../types"
+import { useMenu } from '../../contexts/menu/MenuProvider';
+import { useRef } from 'react';
+import { useAuth } from '../../contexts/auth/AuthProvider';
 
-export const NavbarProfile: React.FC<User> = ({ username }) => {
+export const NavbarProfile: React.FC<User> = ({ avatar }) => {
+    const { profile } = useAuth();
+    const { setMenu } = useMenu();
+    const ref = useRef<HTMLDivElement>(null);
+
+    const openMenu = () => {
+        const logout = () => {
+            localStorage.removeItem('token');
+            window.location.reload();
+        }
+
+        setMenu(
+            [
+                [
+                    { text: 'Profile', href: `/users/${profile?.id}` },
+                    { text: 'Settings', href: `/settings` }
+                ],
+                [
+                    { text: 'Log out', onClick: logout, type: 'danger' }
+                ]
+            ],
+            ref
+        )
+    }
+
     return(
-        <div>
-            {username}
+        <div 
+            className={styles['avatar']} 
+            onClick={openMenu}
+            ref={ref}
+        >
+            <Image 
+                src={`${process.env.NEXT_PUBLIC_AVATAR_ENDPOINT}/${avatar}`}
+                width={32}
+                height={32}
+            />
         </div>
     )
 }
