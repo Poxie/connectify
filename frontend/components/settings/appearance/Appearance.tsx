1f34385c164417d8891f4b077c2e8c0ff45f3e9f
Poxie <poxen12@hotmail.com>
2022-10-18 22:01:05 +0200

    Added simple page to change themes

--- frontend/components/settings/appearance/Appearance.tsx
+++ frontend/components/settings/appearance/Appearance.tsx
@@ -1,7 +1,16 @@
+import styles from '../../../styles/Appearance.module.scss';
+import { SettingsSection } from '../SettingsSection';
+import { AppearanceThemes } from './AppearanceThemes';
+
 export const Appearance = () => {
     return(
-        <div>
-            appearance
+        <div className={styles['container']}>
+            <h2 className={styles['title']}>
+                Appearance
+            </h2>
+            <SettingsSection title={'Themes'}>
+                <AppearanceThemes />
+            </SettingsSection>
         </div>
     )
 }
