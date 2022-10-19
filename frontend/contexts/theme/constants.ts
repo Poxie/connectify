9f9ca111d1a6ee3c73b20c9168b82482e5687186
Poxie <poxen12@hotmail.com>
2022-10-19 15:40:42 +0200

    Added more colors

--- frontend/contexts/theme/constants.ts
+++ frontend/contexts/theme/constants.ts
@@ -1,12 +1,24 @@
 // Color related constants
 export const COLORS = {
     blue: {
-        'primary': '#008FA2',
-        'secondary': '#006F7E'
+        primary: '#008FA2',
+        secondary: '#006F7E'
     },
     orange: {
-        'primary': '#ff7700',
-        'secondary': '#de6902'
+        primary: '#ff7700',
+        secondary: '#de6902'
+    },
+    purple: {
+        primary: '#bb00ff',
+        secondary: '#9d00d6'
+    },
+    red: {
+        primary: '#f20025',
+        secondary: '#e00022'
+    },
+    pink: {
+        primary: '#ff00bf',
+        secondary: '#d900a2'
     }
 }
 export const DEFAULT_COLOR = 'blue';
