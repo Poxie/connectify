2d37c3b69b4644fa7694da426bffd6aafb2220de
Poxie <poxen12@hotmail.com>
2022-10-20 15:14:31 +0200

    Opening login modal on sign in button click

--- frontend/components/navbar/NavbarLoginButton.tsx
+++ frontend/components/navbar/NavbarLoginButton.tsx
@@ -1,10 +1,13 @@
 import { useTranslation } from "next-i18next"
+import { useModal } from "../../contexts/modal/ModalProvider";
+import { LoginModal } from "../../modals/login/LoginModal";
 import Button from "../button"
 
 export const NavbarLoginButton = () => {
     const { t } = useTranslation('common');
+    const { setModal } = useModal();
     return(
-        <Button>
+        <Button onClick={() => setModal(<LoginModal />)}>
             {t('navbar.signIn')}
         </Button>
     )
