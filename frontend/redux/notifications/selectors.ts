d989a9823e8554c851b8a156f1d5121b141b4111
Poxie <poxen12@hotmail.com>
2022-10-23 15:30:53 +0200

    Added selecting notification by id

--- frontend/redux/notifications/selectors.ts
+++ frontend/redux/notifications/selectors.ts
@@ -1,5 +1,14 @@
+import { createSelector } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 
 export const selectNotificationIds = (state: RootState) => state.notifications.notifications.map(notif => notif.id);
 export const selectNotificationsLoading = (state: RootState) => state.notifications.loading;
 export const selectUnreadCount = (state: RootState) => state.notifications.unreadCount;
+
+const selectNotifications = (state: RootState) => state.notifications.notifications;
+const selectId = (_:any, id: number) => id;
+
+export const selectNotificationById = createSelector(
+    [selectNotifications, selectId],
+    (notifications, id) => notifications.find(notif => notif.id === id)
+)
