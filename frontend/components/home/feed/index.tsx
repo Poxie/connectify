5adc9c60bb85c80a1f570df22e3fedba7cf2c82a
Poxie <poxen12@hotmail.com>
2022-10-23 21:51:42 +0200

    Added infinite scrolling to feed

--- frontend/components/home/feed/index.tsx
+++ frontend/components/home/feed/index.tsx
@@ -1,6 +1,6 @@
 import styles from '../../../styles/Feed.module.scss';
 import { motion } from 'framer-motion';
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useAuth } from "../../../contexts/auth/AuthProvider";
 import { useAppDispatch, useAppSelector } from '../../../redux/store';
 import { selectFeedPostIds } from '../../../redux/feed/hooks';
@@ -8,16 +8,24 @@ import { FeedPost } from './FeedPost';
 import { UserPostSkeleton } from '../../user-post/UserPostSkeleton';
 import { AnimatePresence } from 'framer-motion';
 import { setPosts } from '../../../redux/posts/actions';
-import { setFeedPostIds } from '../../../redux/feed/actions';
+import { addFeedPostIds, setFeedPostIds } from '../../../redux/feed/actions';
 import { Post } from '../../../types';
 import { FeedEmpty } from './FeedEmpty';
 import { LoginPrompt } from '../../login-prompt/LoginPrompt';
 
+const SCROLL_THRESHOLD = 500;
 export const Feed = () => {
     const { get, token, loading } = useAuth();
     const dispatch = useAppDispatch();
     const postIds = useAppSelector(selectFeedPostIds);
     const [feedLoading, setFeedLoading] = useState(true);
+    const [reachedEnd, setReachedEnd] = useState(false);
+    const fetching = useRef(false);
+
+    // Function to get feed posts
+    const getFeedPosts = useCallback(async (amount=10, startAt=0) => {
+        return await get(`/feed?amount=${amount}&start_at=${startAt}`) as Promise<Post[]>;
+    }, [get]);
 
     // Initial fetch for feed posts 
     useEffect(() => {
@@ -32,13 +40,39 @@ export const Feed = () => {
 
         setFeedLoading(true);
 
-        get(`/feed`)
-            .then((posts: Post[]) => {
+        getFeedPosts()
+            .then(posts => {
                 dispatch(setFeedPostIds(posts.map(post => post.id)));
                 dispatch(setPosts(posts));
                 setFeedLoading(false);
             })
     }, [get, token, loading]);
+
+    // Loading more posts on scroll
+    useEffect(() => {
+        const onScroll = () => {
+            if(fetching.current) return;
+
+            const diffFromBottom = Math.abs(window.scrollY + window.innerHeight - document.body.offsetHeight);
+            
+            if(diffFromBottom < SCROLL_THRESHOLD) {
+                fetching.current = true;
+
+                getFeedPosts(10, postIds.length)
+                    .then(posts => {
+                        if(!posts.length) return setReachedEnd(true);
+
+                        dispatch(setPosts(posts));
+                        dispatch(addFeedPostIds(posts.map(post => post.id)));
+                        fetching.current = false;
+                    })
+            }
+        }
+
+        // Setting up event listeners
+        window.addEventListener('scroll', onScroll);
+        return () => window.removeEventListener('scroll', onScroll);
+    }, [postIds.length]);
 
     return(
         <div className={styles['container']}>
@@ -69,6 +103,12 @@ export const Feed = () => {
             <AnimatePresence>
                 {postIds.map(id => <FeedPost id={id} key={id} />)}
             </AnimatePresence>
+
+            {reachedEnd && (
+                <span className={styles['end']}>
+                    You have reached the end of your feed.
+                </span>
+            )}
         </div>
     )
 }
