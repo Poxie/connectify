952f4ecfabe15a52971fbae8f9eb8b0519e6a51f
Poxie <poxen12@hotmail.com>
2022-10-23 16:46:24 +0200

    Resetting notification count on notifications read

--- frontend/components/notifications/Notifications.tsx
+++ frontend/components/notifications/Notifications.tsx
@@ -1,14 +1,15 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useAuth } from "../../contexts/auth/AuthProvider";
-import { setNotifications } from "../../redux/notifications/actions";
-import { selectNotificationsLoading, selectNotificationIds } from "../../redux/notifications/selectors"
+import { setNotificationCount, setNotifications } from "../../redux/notifications/actions";
+import { selectNotificationsLoading, selectNotificationIds, selectUnreadCount } from "../../redux/notifications/selectors"
 import { useAppSelector } from "../../redux/store"
 import { Notification } from "./Notification";
 
 export const Notifications = () => {
-    const { token, get, loading } = useAuth();
+    const { token, get, patch, loading } = useAuth();
     const dispatch = useDispatch();
+    const notificationCount = useAppSelector(selectUnreadCount);
     const notificationIds = useAppSelector(selectNotificationIds);
     const notificationsLoading = useAppSelector(selectNotificationsLoading);
 
@@ -20,7 +21,17 @@ export const Notifications = () => {
             .then(notifications => {
                 dispatch(setNotifications(notifications));
             })
-    }, [token, get, loading]);
+    }, [token, get, patch, loading]);
+
+    // Updating notification count
+    useEffect(() => {
+        if(notificationsLoading || !notificationIds.length || !notificationCount) return;
+        
+        patch(`/notifications/reset`)
+            .then(() => {
+                dispatch(setNotificationCount(0));
+            })
+    }, [notificationsLoading, notificationIds, notificationCount]);
 
     return(
         <>
