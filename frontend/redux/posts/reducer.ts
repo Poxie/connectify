60b12eb91e24e1466c41ababdb282304ead227fb
Poxie <poxen12@hotmail.com>
2022-10-29 15:48:26 +0200

    Created utility functions for common operations

--- frontend/redux/posts/reducer.ts
+++ frontend/redux/posts/reducer.ts
@@ -2,27 +2,37 @@ import { Comment, Post } from "../../types";
 import { ADD_POST_COMMENT, ADD_POST_LIKE, REMOVE_POST, REMOVE_POST_LIKE, SET_POST, SET_POSTS, SET_POST_COMMENTS } from "./constants"
 import { PostsReducer } from "./types"
 
-const initialState = {
-    posts: [],
-    comments: []
+function updateObject<T>(oldObject: T, newObject: Partial<T>): T {
+    return Object.assign({}, oldObject, newObject);
 }
 
-export const postsReducer: PostsReducer = (state=initialState, action) => {
+function updateItemInArray<T>(array: (T & { id: number })[], itemId: number, updateItemCallback: (item: T) => T) {
+    const updatedItems = array.map(item => {
+        if(item.id !== itemId) return item;
+
+        const updatedItem = updateItemCallback(item);
+        return updatedItem;
+    })
+    return updatedItems;
+}
+
+export const postsReducer: PostsReducer = (state={
+    posts: [],
+    comments: []
+}, action) => {
     switch(action.type) {
         case SET_POST: {
             const post: Post = action.payload;
             post.hasCommentsFetched = false;
 
-            return Object.assign({}, state, {
-                posts: state.posts.concat(post)
-            })
+            const newPosts = state.posts.concat(post);
+
+            return updateObject(state, { posts: newPosts });
         }
         case REMOVE_POST: {
-            const postId = action.payload;
+            const newPosts = state.posts.filter(post => post.id !== action.payload);
 
-            return Object.assign({}, state, {
-                posts: state.posts.filter(post => post.id !== postId)
-            })
+            return updateObject(state, { posts: newPosts })
         }
         case SET_POST_COMMENTS: {
             // Destructuring payload
@@ -31,67 +41,60 @@ export const postsReducer: PostsReducer = (state=initialState, action) => {
                 postId: number;
             } = action.payload;
 
-            return Object.assign({}, state, {
-                posts: state.posts.map(post => {
-                    if(post.id !== postId) return post;
+            const newPosts = updateItemInArray(state.posts, postId, post => {
+                return updateObject(post, { hasCommentsFetched: true })
+            })
+            const newComments = state.comments.concat(comments);
 
-                    return Object.assign({}, post, {
-                        hasCommentsFetched: true
-                    })
-                }),
-                comments: state.comments.concat(comments)
+            return updateObject(state, {
+                posts: newPosts,
+                comments: newComments
             })
         }
         case ADD_POST_COMMENT: {
             const comment: Comment = action.payload;
 
-            return Object.assign({}, state, {
-                // Increasing comment count
-                posts: state.posts.map(post => {
-                    if(post.id !== comment.post_id) return post;
+            const newPosts = updateItemInArray(state.posts, comment.post_id, post => {
+                return updateObject(post, {
+                    comment_count: post.comment_count + 1
+                })
+            })
+            const newComments = state.comments.concat(comment)
 
-                    return Object.assign({}, post, {
-                        comment_count: post.comment_count + 1
-                    })
-                }),
-                // Adding comment
-                comments: state.comments.concat(comment)
+            return updateObject(state, {
+                posts: newPosts,
+                comments: newComments
             })
         }
         case SET_POSTS: {
-            const posts: Post[] = action.payload;
+            const posts = action.payload;
+            const newPosts = state.posts.concat(posts);
 
-            return Object.assign({}, state, {
-                posts: state.posts.concat(posts)
-            })
+            return updateObject(state, { posts: newPosts })
         }
         case ADD_POST_LIKE: {
             const postId = action.payload;
-            
-            return Object.assign({}, state, {
-                posts: state.posts.map(post => {
-                    if(post.id !== postId) return post;
 
-                    return Object.assign({}, post, {
-                        like_count: post.like_count + 1,
-                        has_liked: true
-                    })
+            const newPosts = updateItemInArray(state.posts, postId, post => {
+                return updateObject(post, {
+                    like_count: post.like_count + 1,
+                    has_liked: true
                 })
             })
+            
+            return updateObject(state, { posts: newPosts });
         }
         case REMOVE_POST_LIKE: {
             const postId = action.payload;
 
-            return Object.assign({}, state, {
-                posts: state.posts.map(post => {
-                    if(post.id !== postId) return post;
-
-                    return Object.assign({}, post, {
-                        like_count: post.like_count - 1,
-                        has_liked: !post.has_liked
-                    })
+            const newPosts = updateItemInArray(state.posts, postId, post => {
+                return updateObject(post, {
+                    like_count: post.like_count - 1,
+                    has_liked: false
                 })
             })
+
+            return updateObject(state, { posts: newPosts })
         }
         default:
             return state;
