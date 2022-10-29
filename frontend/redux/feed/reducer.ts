913a3122104896ee86c01cdf318000c372758134
Poxie <poxen12@hotmail.com>
2022-10-29 19:06:23 +0200

    Moved reducers to new reducer system

--- frontend/redux/feed/reducer.ts
+++ frontend/redux/feed/reducer.ts
@@ -1,26 +1,26 @@
+import { AnyAction } from "redux";
+import { createReducer, updateObject } from "../utils";
 import { ADD_FEED_POST_IDS, SET_FEED_POST_IDS } from "./constants"
 import { FeedState, Reducer } from "./types"
 
-const initialState = {
+// Reducer actions
+type ReducerAction = (state: FeedState, action: AnyAction) => FeedState;
+
+const setFeedPostIds: ReducerAction = (state, action) => {
+    return updateObject(state, { postIds: action.payload })
+}
+
+const addFeedPostIds: ReducerAction = (state, action) => {
+    return updateObject(state, {
+        postIds: state.postIds.concat(action.payload)
+    })
+}
+
+// Creating reducer
+export const feedReducer = createReducer({
     postIds: [],
     loading: true
-} as FeedState;
-
-export const feedReducer: Reducer = (state=initialState, action) => {
-    switch(action.type) {
-        case SET_FEED_POST_IDS: {
-            return {
-                ...state,
-                postIds: action.payload
-            }
-        }
-        case ADD_FEED_POST_IDS: {
-            return {
-                ...state,
-                postIds: [...state.postIds, ...action.payload]
-            }
-        }
-        default:
-            return state;
-    }
-}
+}, {
+    SET_FEED_POST_IDS: setFeedPostIds,
+    ADD_FEED_POST_IDS: addFeedPostIds
+})
