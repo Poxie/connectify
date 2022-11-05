62ecb1b55570ed9e3d32d7c875bc4ac16683f9a8
Poxie <poxen12@hotmail.com>
2022-11-05 01:11:16 +0100

    Updated user post skeleton

--- frontend/components/user-post/UserPostSkeleton.tsx
+++ frontend/components/user-post/UserPostSkeleton.tsx
@@ -7,13 +7,27 @@ export const UserPostSkeleton = () => {
             initial={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             className={styles['skeleton']}
+            aria-hidden="true"
         >
-            <div className={styles['skeleton-flex']}>
-                <div className={styles['skeleton-name']} />
-                <div className={styles['skeleton-timestamp']} />
+            <div className={styles['header']}>
+                <div className={styles['skeleton-flex']}>
+                    <div className={styles['skeleton-avatar']} />
+                    <div className={styles['skeleton-name']} />
+                    <div className={styles['skeleton-timestamp']} />
+                </div>
+                <div className={styles['skeleton-options']} />
             </div>
-            <div className={styles['skeleton-content']} />
+            <div className={styles['skeleton-content']}>
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+            </div>
             <div className={styles['skeleton-footer']}>
+                <div className={styles['skeleton-footer-button']} />
                 <div className={styles['skeleton-footer-button']} />
             </div>
         </motion.div>
