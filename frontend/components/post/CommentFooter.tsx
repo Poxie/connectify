f68bca78e56b58024b82f29df05ee0ec0a1a2c67
= <poxen12@hotmail.com>
2022-11-10 19:29:55 +0100

    Added login check

--- frontend/components/post/CommentFooter.tsx
+++ frontend/components/post/CommentFooter.tsx
@@ -3,6 +3,8 @@ import { useDispatch } from 'react-redux';
 import { HeartActiveIcon } from '../../assets/icons/HeartActiveIcon';
 import { HeartNeutralIcon } from '../../assets/icons/HeartNeutralIcon';
 import { useAuth } from '../../contexts/auth/AuthProvider';
+import { useModal } from '../../contexts/modal/ModalProvider';
+import { LoginModal } from '../../modals/login/LoginModal';
 import { addCommentLike, removeCommentLike } from '../../redux/posts/actions';
 import { selectCommentStats } from '../../redux/posts/selectors';
 import { useAppSelector } from '../../redux/store';
@@ -13,7 +15,8 @@ export const CommentFooter: React.FC<{
     id: number;
 }> = ({ id }) => {
     const { t } = useTranslation('common');
-    const { post, destroy } = useAuth();
+    const { token, loading, post, destroy } = useAuth();
+    const { setModal } = useModal();
     const dispatch = useDispatch();
     const stats = useAppSelector(state => selectCommentStats(state, id));
     if(!stats) return null;
@@ -21,14 +24,22 @@ export const CommentFooter: React.FC<{
     const { has_liked, like_count } = stats;
 
     const toggleLike = async () => {
+        // User is not logged in
+        if(!token && !loading) {
+            setModal(<LoginModal />);
+            return;
+        }
+
         const path = `/comments/${id}/likes`;
 
+        // Destroying like
         if(has_liked) {
             destroy(path);
             dispatch(removeCommentLike(id));
             return
         }
 
+        // Creating like
         post(path);
         dispatch(addCommentLike(id));
     }
