7e1a82e0479003729b5d56e449b3e8d141020366
= <poxen12@hotmail.com>
2022-11-10 16:37:25 +0100

    Added domain allow to images

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ const { i18n } = require('./next-i18next.config');
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost']
+    domains: ['localhost', '127.0.0.1']
   },
   async redirects() {
     return[
