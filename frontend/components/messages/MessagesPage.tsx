555522a6079315ea8b55f8274c9c2e038fdc37dc
= <poxen12@hotmail.com>
2022-11-12 21:58:08 +0100

    Added unauthorized label

--- frontend/components/messages/MessagesPage.tsx
+++ frontend/components/messages/MessagesPage.tsx
@@ -1,5 +1,6 @@
+import { useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
-import { selectChannelById } from '../../redux/messages/selectors';
+import { selectChannelById, selectChannelsLoading } from '../../redux/messages/selectors';
 import { useAppSelector } from '../../redux/store';
 import styles from '../../styles/Messages.module.scss';
 import { MessageInput } from "./MessageInput"
@@ -7,9 +8,19 @@ import { Messages } from "./Messages"
 import { MessagesHeader } from './MessagesHeader';
 
 export const MessagesPage = () => {
+    const { t } = useTranslation('messages');
     const { channelId } = useRouter().query as { channelId: string };
     const channel = useAppSelector(state => selectChannelById(state, parseInt(channelId)));
+    const channelsLoading = useAppSelector(selectChannelsLoading);
 
+    // If user has no access, return label
+    if(!channel && !channelsLoading) {
+        return(
+            <span className={styles['label']}>
+                {t('unauthorized')}
+            </span>
+        )
+    };
     if(!channel) return null;
 
     const recipient = channel.recipients[0];
