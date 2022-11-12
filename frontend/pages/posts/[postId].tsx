d4cf8f5fe98157d870a99240c60b1a8e5a484253
= <poxen12@hotmail.com>
2022-11-12 22:21:22 +0100

    Adding basic not found post notice

--- frontend/pages/posts/[postId].tsx
+++ frontend/pages/posts/[postId].tsx
@@ -1,17 +1,36 @@
 import { GetServerSideProps } from "next";
+import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { Post } from "../../components/post"
+import { useToast } from "../../contexts/toast/ToastProvider";
 import { Post as PostType } from "../../types"
 
 export default function PostPage({ post }: {
     post: PostType | undefined;
 }) {
+    const { t } = useTranslation('post');
+    const { setToast } = useToast();
+    const router = useRouter();
+    
+    useEffect(() => {
+        if(!post) {
+            setToast(t('postNotFound'), 'error');
+        }
+    }, [post]);
+
+    const title = post ? (
+        `${post?.author.display_name || post?.author.username}: ${post?.title}`
+    ) : (
+        t('postNotFound')
+    )
     return(
         <>
         <Head>
             <title>
-                {post?.author.display_name || post?.author.username}: {post?.title} - {process.env.NEXT_PUBLIC_WEBSITE_NAME}
+                {title} - {process.env.NEXT_PUBLIC_WEBSITE_NAME}
             </title>
             <meta property="og:description" content={post?.content} />
             <meta property="og:site_name" content={process.env.NEXT_PUBLIC_WEBSITE_NAME} />
@@ -27,7 +46,7 @@ export default function PostPage({ post }: {
 export const getServerSideProps: GetServerSideProps = async ({ locale, query: { postId } }) => {
     const translations = await serverSideTranslations(locale || process.env.NEXT_PUBLIC_DEFAULT_LOCALE, ['common', 'post']);
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/posts/${postId}`).catch(e => console.error('this is error:', e));
-    if(!res) return {
+    if(!res || !res.ok) return {
         props: { ...translations }
     };
 
