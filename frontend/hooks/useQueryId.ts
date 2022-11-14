5f78622c61d6c1c742efcad3eb11fd1851db23ae
= <poxen12@hotmail.com>
2022-11-14 23:37:12 +0100

    Adding extra checks for url search

--- frontend/hooks/useQueryId.ts
+++ frontend/hooks/useQueryId.ts
@@ -1,6 +1,8 @@
 import { useRouter } from "next/router";
 
-const MANUAL_PARAM_IDS = ['postId'];
+const MANUAL_PARAMS = [{id: 'postId', prefix: 'posts'}];
+const MANUAL_PARAM_IDS = MANUAL_PARAMS.map(param => param.id);
+const getManualParam = (id: string) => MANUAL_PARAMS.find(param => param.id === id);
 
 const cache: { [key: string]: number } = {};
 export const useQueryId = (queryId: string) => {
@@ -14,11 +16,12 @@ export const useQueryId = (queryId: string) => {
             return cache[queryId];
         }
     }
-    if(!MANUAL_PARAM_IDS.includes(queryId)) return 0;
+    if(!MANUAL_PARAM_IDS.includes(queryId)) return;
 
     // Checking URL params
     const asPath = useRouter().asPath;
     const parts = asPath.split('/');
+    if(parts[1] !== getManualParam(queryId)?.prefix) return;
     const postId = parseInt(parts[2]?.split('?')[0]);
     return postId;
 }
