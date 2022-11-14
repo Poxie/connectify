ddfa821fc540a6e674ffb260714c477c3edb1217
= <poxen12@hotmail.com>
2022-11-14 11:17:35 +0100

    Providing comments component with scroll container

--- frontend/overlays/post/Main.tsx
+++ frontend/overlays/post/Main.tsx
@@ -1,14 +1,17 @@
 import styles from './PostOverlay.module.scss';
 import { PostMain } from '../../components/post/PostMain';
 import { Comments } from '../../components/post/Comments';
+import { useRef } from 'react';
 
 export const Main: React.FC<{
     postId: number;
 }> = ({ postId }) => {
+    const ref = useRef<HTMLDivElement>(null);
+
     return(
-        <div className={styles['main']}>
+        <div className={styles['main']} ref={ref}>
             <PostMain />
-            <Comments />
+            <Comments containerRef={ref} />
         </div>
     )
 }
