f102c7bf2105a9bba869d64b769ec7c764c28600
= <poxen12@hotmail.com>
2022-11-14 21:55:19 +0100

    If router query is not present, check url manually

--- frontend/hooks/useQueryId.ts
+++ frontend/hooks/useQueryId.ts
@@ -1,6 +1,13 @@
 import { useRouter } from "next/router";
 
 export const useQueryId = (queryId: string) => {
+    // Checking next router query
     const id = useRouter().query[queryId] as string;
-    return parseInt(id);
+    if(id) return parseInt(id);
+
+    // Checking URL params
+    const asPath = useRouter().asPath;
+    const parts = asPath.split('/');
+    const postId = parseInt(parts[2].split('?')[0]);
+    return postId;
 }
