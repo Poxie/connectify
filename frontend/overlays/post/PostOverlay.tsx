4026f9d6314b9e096e6615fd41e343e5195b30f8
= <poxen12@hotmail.com>
2022-11-14 18:16:45 +0100

    Increased responsiveness on smaller devices

--- frontend/overlays/post/PostOverlay.tsx
+++ frontend/overlays/post/PostOverlay.tsx
@@ -2,16 +2,20 @@ import styles from './PostOverlay.module.scss';
 import { Attachment } from './Attachment';
 import { Main } from './Main';
 import { CloseOverlay } from './CloseOverlay';
+import { useScreenType } from '../../hooks/useScreenType';
 
 export const PostOverlay: React.FC<{
     postId: number;
     attachmentIndex?: number;
 }> = ({ postId, attachmentIndex }) => {
+    const screenType = useScreenType();
     return(
         <div className={styles['container']}>
             <CloseOverlay postId={postId} />
             <Attachment postId={postId} defaultIndex={attachmentIndex || 0} />
-            <Main postId={postId} />
+            {['largium', 'large'].includes(screenType) && (
+                <Main postId={postId} />
+            )}
         </div>
     )
 }
