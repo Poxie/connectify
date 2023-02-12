90cfb45307468083263572746f072b95b98d950e
= <poxen12@hotmail.com>
2023-02-12 19:28:36 +0100

    Allowing to opt-out of cache

--- frontend/hooks/useQueryId.ts
+++ frontend/hooks/useQueryId.ts
@@ -1,12 +1,12 @@
 import { useRouter } from "next/router";
 
 const cache: { [key: string]: number } = {};
-export const useQueryId = (queryId: string) => {
+export const useQueryId = (queryId: string, withCache=true) => {
     const id = useRouter().query[queryId] as string;
     if(id) {
         cache[queryId] = parseInt(id);
         return parseInt(id);
-    } else {
+    } else if(withCache) {
         return cache[queryId];
     }
 }
